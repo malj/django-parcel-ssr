82b5829e5e5ebf02c0a457a24210ac6d49b27819
Luka Maljic <luka@maljic.com>
2019-03-19 10:00:36 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='django-parcel-ssr',
-    version='0.0.2',
+    version='0.0.3',
     author='Luka Maljic',
     author_email='luka@maljic.com',
     description='Django server side rendering, powered by Parcel bundler',
